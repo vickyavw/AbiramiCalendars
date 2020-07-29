56f9de1e9744ff2ceed528fd142bcc60ea86c8e9
vickyavw <vickyavw@gmail.com>
2020-07-29 20:43:10 +0530

    Removing the dummmy app

--- Webportal/src/java/com/abirami/web/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.abirami.web;
-
-public class HelloWorld {
-	   public static void main(String[] args) {
-	      // Prints "Hello World" in the terminal window.
-	      System.out.println("Hello World.");
-	   }
-	}
--- Webportal/src/test/com/abirami/web/HelloWorldTest.java
+++
@@ -1,5 +0,0 @@
-package com.abirami.web;
-
-public class HelloWorldTest {
-
-}
