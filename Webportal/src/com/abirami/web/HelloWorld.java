9d475a74264dda43a8c7629587fe42fcffe10c1a
Vigneshwaran Anbarasan <vanbarasan@in.ibm.com>
2020-07-29 12:00:54 +0530

    Testing

--- Webportal/src/com/abirami/web/HelloWorld.java
+++ Webportal/src/com/abirami/web/HelloWorld.java
@@ -3,6 +3,6 @@ package com.abirami.web;
 public class HelloWorld {
 	   public static void main(String[] args) {
 	      // Prints "Hello World" in the terminal window.
-	      System.out.println("Hello World");
+	      System.out.println("Hello World.");
 	   }
 	}
