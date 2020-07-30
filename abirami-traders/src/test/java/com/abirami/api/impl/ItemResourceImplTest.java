f11d92b506134ab0c21a114e8b40f9ebe1fc33a0
vickyavw <vickyavw@gmail.com>
2020-07-30 21:52:24 +0530

    Creating rest APIs

---
+++ abirami-traders/src/test/java/com/abirami/api/impl/ItemResourceImplTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.abirami.api.impl;
+
+import org.junit.Test;
+
+/**
+ * @author vicky
+ *
+ */
+public class ItemResourceImplTest {
+	@Test
+	public void test_getItems() {
+		new ItemResourceImpl().getItems();
+	}
+}
