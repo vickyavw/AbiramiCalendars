73f348974c9b70c4f1140c99312326f7c67dbeb7
Giri-M10 <giridharan95@gmail.com>
2020-07-30 00:45:42 -0400

    Added Admin Section (temp)

---
+++ abirami-traders/src/main/java/com/abirami/admin/AbiramiAction.java
@@ -0,0 +1,11 @@
+package com.abirami.admin;
+
+public class AbiramiAction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to Admin Section");
+
+	}
+
+}
