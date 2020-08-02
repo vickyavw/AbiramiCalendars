6b12538c49935e8e985a49f2a2d303df28e6979c
vickyavw <vickyavw@gmail.com>
2020-08-03 02:23:30 +0530

    Renaming item table to products

--- init-setup.sql
+++ init-setup.sql
@@ -1,9 +1,10 @@
 create database abirami_traders;
+use abirami_traders;
 --The user to be used in hibernate
 create user 'springuser'@'%' identified by 'admin@123';
 
 grant all on abirami_traders.* to 'springuser'@'%';
 
-create table item(item_id INT not null auto_increment, display_name varchar(100) not null, description varchar(100), image MEDIUMBLOB, price decimal, availability_count int, time_to_print int, primary key (item_id));
+create table products(product_id INT not null auto_increment, display_name varchar(100) not null, description varchar(100), image MEDIUMBLOB, price decimal, availability_count int, time_to_print int, primary key (product_id));
 
-insert into item (display_name, description, price, availability_count, time_to_print) values ('Calendar1','Test Calendar 1','12.5','1000','5');
+insert into products(display_name, description, price, availability_count, time_to_print) values ('Calendar1','Test Calendar 1','12.5','1000','5');
